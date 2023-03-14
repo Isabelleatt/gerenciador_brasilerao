1688af8e595e62688cbfa56bc29cb0a35bc46545
esdrasgc <esdrasgcdev@gmail.com>
2023-03-14 10:15:25 -0300

    create maven project

---
+++ gerenciadorBrasileirao/src/main/java/com/insper/gerenciadorBrasileirao/GerenciadorBrasileiraoApplication.java
@@ -0,0 +1,13 @@
+package com.insper.gerenciadorBrasileirao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorBrasileiraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorBrasileiraoApplication.class, args);
+	}
+
+}
---
+++ gerenciadorBrasileirao/src/test/java/com/insper/gerenciadorBrasileirao/GerenciadorBrasileiraoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insper.gerenciadorBrasileirao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorBrasileiraoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
